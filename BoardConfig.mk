ab11530166bb0acf6b9f5b2bd171f8ff92e9473c
daniel_hk <daniel.p6800@gmail.com>
2021-03-17 21:31:33 +0100

    ac50fnev2: include: override audio.h
    * Use our own audio.h to match the MTK AV blobs
    
    Change-Id: I80f6e5db2700a145590db4292a0a4b0d2f273830

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,9 @@ TARGET_HARDWARE_3D := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 
+# Include
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1 androidboot.selinux=permissive
